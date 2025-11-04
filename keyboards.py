f628b92ca9a773c51222a63dd86d8fd1e11756d4
leonova-78@mail.ru <leonova-78@mail.ru>
2025-11-04 15:19:15 +0300

    Initial commit of the project

--- keyboards.py
+++ keyboards.py
@@ -1,8 +1,8 @@
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, WebAppInfo
 
 def webAppKeyboard():
-    web_app_info = WebAppInfo(url="https://secretly-tenable-marlin.cloudpub.ru//")
-    web_app_button = KeyboardButton(text="Тестовая страница", web_app=web_app_info)
+    web_app_info = WebAppInfo(url="https://stably-aboveboard-vireo.cloudpub.ru/")
+    web_app_button = KeyboardButton(text="Запуск приложения", web_app=web_app_info)
 
     keyboard = ReplyKeyboardMarkup(
         keyboard=[[web_app_button]],  
